7e1988e7f45d0b7c176a1b66c87a07c95f152c7a
dabauxi <dabauxi@users.noreply.github.com>
2022-07-31 21:52:39 +0200

    Move comment up#

--- scripts/start-nethermind.sh
+++ scripts/start-nethermind.sh
@@ -9,17 +9,18 @@ if [ "$EXECUTION_NETWORK" != "" ]; then
     DEFAULT_NETWORK=$EXECUTION_NETWORK
 fi
 
+#--JsonRpc.AdditionalRpcUrls="http://0.0.0.0:8551|http;ws|engine;eth;subscribe" \
+#--JsonRpc.JwtSecretFile="/secrets/jwtsecret" \
+
 exec ./Nethermind.Runner \
-    --config $DEFAULT_NETWORK \
+    --config "$DEFAULT_NETWORK" \
     --Sync.SnapSync true \
     --JsonRpc.Enabled true \
     --JsonRpc.EnabledModules "Admin,Web3,Eth,Subscribe,Net" \
     --JsonRpc.Host 0.0.0.0 \
     --JsonRpc.Port 8545 \
-    #--JsonRpc.AdditionalRpcUrls="http://0.0.0.0:8551|http;ws|engine;eth;subscribe" \
-    #--JsonRpc.JwtSecretFile="/secrets/jwtsecret" \
     --Init.WebSocketsEnabled true \
-    --datadir /nethermind/data \
+    --datadir data \
     --Pruning.Mode "Full" \
     --Pruning.CacheMb "4096" \
     --Sync.AncientBodiesBarrier "11052984" \
