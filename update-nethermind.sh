681832d22a4df1c55cb56f0a7a736c3cb2666d28
dabauxi <dabauxi@users.noreply.github.com>
2022-08-13 16:49:28 +0200

    Remove client specific update script

--- update-nethermind.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-docker-compose pull nethermind
-# stop and remove containers
-docker-compose stop nethermind && yes | docker-compose rm -v nethermind
-docker-compose up -d nethermind
--- update-nimbus.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-docker-compose pull nimbus
-# stop and remove containers
-docker-compose stop nimbus && yes | docker-compose rm -v nimbus
-docker-compose up -d nimbus
