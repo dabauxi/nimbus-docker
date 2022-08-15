d21eb00318701996b09324747a6bd02630caa5b6
dabauxi <dabauxi@users.noreply.github.com>
2022-08-15 13:46:57 +0200

    Improve update script

--- update.sh
+++ update.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
+# Update docker services.
+# Either update all services or the services you name.
+# e.g. ./update.sh nimbus nethermind
+
+if [ "$#" -eq 0 ]; then
+    services="nimbus nethermind"
+fi
+
+services=$@
 
 docker-compose pull
-docker-compose down
-docker-compose up -d
+docker-compose stop $services && yes | docker-compose rm -v $services
+docker-compose up -d $@
