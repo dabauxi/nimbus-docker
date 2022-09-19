62dadda894c9ff7ab7082294f53e81a1bb658dc2
dabauxi <dabauxi@users.noreply.github.com>
2022-09-19 22:05:33 +0200

    Enable metrics exporter to beaconcha.in

--- start.sh
+++ start.sh
@@ -10,8 +10,13 @@ services="nimbus besu"
 
 if [ "$ENABLE_MEVBOOST" != "" ]; then
     NIMBUS_MEVBOOST_FLAGS="--payload-builder --payload-builder-url=http://mevboost:18550"
+    services="nimbus besu mevboost"
+fi
+
+if [ "$ENABLE_MONITORING" != "" ]; then
     services=""
 fi
+
 
 docker-compose pull
 docker-compose up --remove-orphans -d ${services}
