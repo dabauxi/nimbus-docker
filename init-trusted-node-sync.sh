f89b59d36e8e415b9fc9a53b87c67d82d318234b
dabauxi <dabauxi@users.noreply.github.com>
2022-10-15 11:23:35 +0200

    Fix init trusted node sync, Set example trusted node sync url

--- init-trusted-node-sync.sh
+++ init-trusted-node-sync.sh
@@ -6,4 +6,4 @@ data_dir_path=$(readlink -f ${CONSENSUS_DATA})
 
 docker run -i --rm \
     -v ${data_dir_path}:/home/user/nimbus-eth2/build/data -u $(id -u):$(id -g) \
-    statusim/nimbus-eth2:amd64-v${NIMBUS_VERSION} trustedNodeSync --network=${NETWORK} --data-dir=/home/user/nimbus-eth2/build/data --trusted-node-url=${TRUSTED_NODE_SYNC_URL} --backfill=false
+    statusim/nimbus-eth2:amd64-${NIMBUS_VERSION} trustedNodeSync --network=${NETWORK} --data-dir=/home/user/nimbus-eth2/build/data --trusted-node-url=${TRUSTED_NODE_SYNC_URL} --backfill=false
